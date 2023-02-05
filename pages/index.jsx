7146668916819edf6a34c95672b4a847175424fe
Fabio Diogo <fabindiogo@gmail.com>
2023-02-05 10:55:37 -0300

    outro teste de deploy

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,91 +1,16 @@
 import { useEffect, useState } from 'react'
 import styled from 'styled-components'
-import { withIronSessionSsr } from 'iron-session/next'
-import axios from 'axios'
-import useSWR from 'swr'
 
-import { ironConfig } from '../lib/middlewares/ironSession'
 
-import Navbar from "../src/components/layout/Navbar"
-import Container from '../src/components/layout/Container'
-import CreatePost from '../src/components/cards/CreatePost'
-import H3 from '../src/components/typography/H3'
-import Post from '../src/components/cards/Post'
 
-const Content = styled.div`
-  margin: 50px 0;
-`
-const LastPostText = styled(H3)`
-  padding: 30px 0;
-`
+function HomePage () {
 
-const RefreshPosts =styled.span`
-  font-weight: bold;
-  color: ${props => props.theme.primary};
-  cursor: pointer;
-`
-const RefreshPostsContainer = styled.div`
-  text-align: center;
-  margin-bottom: 20px;
-`
-const PostContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-`
-const fetcher = url => axios.get(url).then(res => res.data)
-
-function HomePage ({user}) {
-  const { data } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, fetcher)
 
   return (
     <> 
-       <Navbar />
-       <Content>
-        <Container>
-          <CreatePost username={user.user} />
-          <LastPostText>Últimas postagens:</LastPostText>
-          <PostContainer>
-              {
-                data?.map(post => 
-                  <Post 
-                    key={post._id}
-                    text={post.text}
-                    user={post.createdBy.user}
-                    date={post.createdDate}
-                    isOwner={post.createdBy._id === user.id}
-                    id={post._id}
-                  />
-                )
-              }
-          </PostContainer>       
-        </Container>     
-       </Content>
+      <p>hello world</p>
     </>   
   )
 }
-
-export const getServerSideProps =  withIronSessionSsr(
-  async function gettServerSideProps({ req }){
-    const user = req.session.user
-
-    if (!user) {
-      return{
-        redirect: {
-          permanent: false,
-          destination: '/login'
-        }
-      }
-    }
-    
-
-    return{
-      props: {
-        user
-      }
-    }
-  },
-  ironConfig
-)
 
 export default HomePage
--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -4,46 +4,11 @@ import styled from 'styled-components';
 
 
 
-const H1 = styled.h1`
-  font-size:85px;
-  color: orange;
-  @media (max-width: 676px){
-    font-size:45px;
-  }
-`
-const DivMain = styled.section`
-  display: flex;
-  flex-direction: column;
-`
-const Content = styled.section`
-  display: flex;
-  flex-direction: column;
-`
-const P = styled.p`
-  display: flex;
-`
-
-interface Props{
-  id: string
-}
-
-function About ({id}: Props){
+function About (){
   return( 
-      <a id={id}>
-      <DivMain>
-        <H1> Sobre mim </H1>
-        <Content>
-          <P> Sou o Fabio e sou da cidade de Sabará-MG, na região da grande BH. Moro com minha família que é são minha mãe e meu irmão.<br />
-            Comecei a gostar de informática quando tinha 12 anos quando ganhei meu primeiro computador.<br /> 1 ano depois entrei no curso de Tecnico em Informática na minha escola do ensino médio, pelo Pronatec.<br />
-            Depois que formei no curso técnico junto com o ensino médio, ainda sem saber ao certo a carreira que eu seguiria, em 2017 comecei a cursar a faculdade de Engenharia Eletrica.<br />
-            No ano seguinte, consegui passar no processo seletivo do IFMG para fazer Sistemas de Informação.<br /> Nesse ponto já sabia que eu queria trabalhar com código, mas não sabia a área específica.<br />
-            E com os projetos feitos em trabalhos de faculdade, descobri o JavaScript que me abriu as portas para o desenvolvimento Front-End, que é a área em que estou me especializando.<br />
-            Sou uma pessoa que cultiva todos os valores de uma pessoa de bom caráter e que ama a sua família acima de tudo. <br />
-            Sou uma pessoa paciente, honesta, justa, proativa , determinada e resiliente , e sempre estou a desenvolver novos hábitos para o meu desenvolvimento pessoal e profissional. 
-          </P> 
-        </Content>         
-      </DivMain>   
-      </a>
+     <>
+      <p>sdf</p>
+     </>
   )
 }
 
